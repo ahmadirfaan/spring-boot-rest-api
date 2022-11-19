ad55ce389d41be59733bb7c8b52fb08475eb8e3b
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2022-11-19 07:19:33 +0700

    spring boot template

---
+++ src/test/java/com/irfaan/danspro/DansproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irfaan.danspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DansproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
